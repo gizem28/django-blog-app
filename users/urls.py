4802e9248e91d833fb425c01e445353c9fae0ebc
gizem28 <gizemodun28@gmail.com>
2022-02-08 14:45:24 +0100

    reset password added

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import register, user_logout, user_login
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', register, name='register' ),
     path('logout/', user_logout, name='logout' ),
     path('login/', user_login, name='user_login' ),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('change-password/',
+    auth_views.PasswordChangeView.as_view(template_name='change-password.html'),
+    ),
 ]
