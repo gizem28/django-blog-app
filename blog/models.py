fb98f062d3cf0ceed59883479fdf083b482d5bda
gizem28 <gizemodun28@gmail.com>
2022-02-09 01:37:54 +0100

    featured

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField
+
+    GENDER =(
+        ("1", "Female"),
+        ("2", "Male"),
+        ("3", "Other"),
+        ("4", "Prefer Not Say"),
+    )
+
+    gender = models.CharField(max_length=50, choices=GENDER)
+    number = models.IntegerField(unique=True, blank=True, null=True)
+    image = models.ImageField(upload_to="student/", default="avatar.png")
+
+    def __str__(self):
+        return f"{self.number} {self.first_name} {self.last_name}"
